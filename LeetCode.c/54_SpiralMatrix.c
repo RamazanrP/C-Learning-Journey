c0cbf0912343e2fc8d5de98cfa36c60fb72ffe75
RamazanrP <ramazanpc23@gmail.com>
2026-01-17 01:37:26 +0300

    Yorum satıralrı eklendi

--- LeetCode.c/54_SpiralMatrix.c
+++ LeetCode.c/54_SpiralMatrix.c
@@ -6,26 +6,26 @@ if (matrixSize == 0 || *matrixColSize == 0) {
     return NULL;
 }    
 int Total = matrixSize * (*matrixColSize); // Toplam eleman adedi hesabı
-int* sonuc = (int*)malloc(Total*sizeof(int));
-int satir = matrixSize, sutun = *matrixColSize;
-int ust=0,alt=satir-1,sag= sutun-1,sol=0,k=0;
+int* sonuc = (int*)malloc(Total*sizeof(int)); // Sonuç dizisi için yer ayırdık
+int satir = matrixSize, sutun = *matrixColSize; // Sütunu bulmak için * ile zarfı açtık
+int ust=0,alt=satir-1,sag= sutun-1,sol=0,k=0; //Sonucu dizmek için k'yı kullanıcaz ve her for içinde manuel arttırmalıyız!
 while(ust<=alt && sol<=sag){
     for(int j=sol;j<=sag;j++){
         sonuc[k] = matrix[ust][j];
         k++;
     }
-    ust++;
+    ust++; // Yukarı ile işimiz bitti, arttırdık. Diğerleri de böyle olacak
     for(int j=ust;j<=alt;j++){
        sonuc[k] = matrix[j][sag];
        k++;
     }
     sag--;
-    if(ust<=alt){
+    if(ust<=alt){ // sola ve aşağı gitmede sorun yok ama sola ve yukarıya çıkarken tekrara düşmemek için if zorunluluk
         for(int j=sag;j>=sol;j--){
             sonuc[k] = matrix[alt][j];
             k++;
         }
-        alt--;
+        alt--; // alt ve sol'u if'in içinde değiştirmeliyiz ki if'e girmeme durumunda bunlara bir şey olmasın. 
     }
     if(sol<=sag){
         for(int j=alt;j>=ust;j--){
